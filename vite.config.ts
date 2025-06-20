cc15a2a64a2e9629bdc5c87af94120890cbad766
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2025-06-20 17:36:12 +0200

    build: setup vite, vue, typescript, tailwind

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  base: '/frontendmentor-testimonials-grid-section/',
+  plugins: [
+    vue(),
+    tailwindcss()
+  ],
+});
